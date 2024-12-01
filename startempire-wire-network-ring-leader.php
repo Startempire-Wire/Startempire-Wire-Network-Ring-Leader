e38d3e5234c0603626c8d2691631e0c45e07b4cc
verious.smith@philoveracity.com <verious.smith@philoveracity.com>
2024-12-01 00:34:20 -0800

    initial plugin commit

---
+++ startempire-wire-network-ring-leader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Startempire Wire Network Ring Leader
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     startempire-wire-network-ring-leader
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Startempire_Wire_Network_Ring_Leader
+ */
+
+// Your code starts here.
