ccb0a1648cf4ce88e769f8b73fb9e730cc6ea152
Verious Smith III <verious.smith@philoveracity.com>
2024-12-01 18:17:20 -0800

    updated plugin description

--- startempire-wire-network-ring-leader.php
+++ startempire-wire-network-ring-leader.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Startempire Wire Network Ring Leader
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://startempirewire.network
+ * Description:     Ring Leader for the Startempire Wire Network. Singularly Unique Plugin that acts as the messenger of The Parent Membership Website of The Network [ https://startempirewire.com ]
+ * Author:          Startempire Wire
+ * Author URI:      https://startempirewire.network
  * Text Domain:     startempire-wire-network-ring-leader
  * Domain Path:     /languages
  * Version:         0.1.0
